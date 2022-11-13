1c1d655ec4f07e6f92c7442f50b9bd6376ac7fa7
RowDog <mrdiamonddirt@gmail.com>
2022-11-13 17:47:06 +0000

    🚧tape for securing doors

--- config.lua
+++ config.lua
@@ -6,6 +6,7 @@ Config.Objects = {
     ["roadsign"] = {model = `prop_snow_sign_road_06g`, freeze = true},
     ["tent"] = {model = `prop_gazebo_03`, freeze = true},
     ["light"] = {model = `prop_worklight_03b`, freeze = true},
+    ["tape"] = {model = `bh1_01_clothtape`, freeze = true},
 }
 
 Config.MaxSpikes = 5
